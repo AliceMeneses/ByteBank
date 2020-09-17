4616dc19f8dbdac33edf31be2f0596e46e90a71a
Alice <am171696@alunos.unisanta.br>
2020-09-17 20:31:32 -0300

    Adicionando classe TestaConexão com o tratamento de exceção

---
+++ src/TestaConexao.java
@@ -0,0 +1,16 @@
+
+public class TestaConexao {
+	
+	public static void main(String[] args) {
+				
+		Conexao con = null;
+		try {
+			con = new Conexao();
+			con.leDados();
+		} catch(IllegalStateException ex) {
+			System.out.println("Falha na conexão");
+		} finally {
+			con.fecha();
+		}		
+	}	
+}
