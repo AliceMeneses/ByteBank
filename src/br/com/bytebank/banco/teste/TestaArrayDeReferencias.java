e92c5627ce33ed1cd2734f604a7dc8d13b57d61b
Alice <am171696@alunos.unisanta.br>
2020-10-09 17:34:55 -0300

    Manipulando array de Object e organizando o código

--- src/br/com/bytebank/banco/teste/TestaArrayDeReferencias.java
+++ src/br/com/bytebank/banco/teste/TestaArrayDeReferencias.java
@@ -1,5 +1,6 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
@@ -8,6 +9,12 @@ import br.com.bytebank.banco.modelo.ContaPoupanca;
 public class TestaArrayDeReferencias {
 
 	public static void main(String[] args) {
+		 
+		/*Array de ContaCorrente*/
+		
+		System.out.println("Array de ContaCorrente");
+		System.out.println();
+		
 		ContaCorrente[] ccs = new ContaCorrente[3];
 		
 		ContaCorrente cc1 = new ContaCorrente(123, 4548);
@@ -29,6 +36,11 @@ public class TestaArrayDeReferencias {
 			ex.printStackTrace();
 		}
 		
+		/*Type Cast de referencias*/
+		
+		System.out.println();
+		System.out.println("Type Cast de referencias");
+		System.out.println();
 		
 		Conta[] contas = new Conta[2];
 		
@@ -49,6 +61,24 @@ public class TestaArrayDeReferencias {
 			ex.printStackTrace();
 		}
 		
+		/*Array de Object*/
+		
+		System.out.println();
+		System.out.println("Array de Object");
+		System.out.println();
+		
+		Object[] ob = new Object[3];
+		
+		ob[0] = new ContaPoupanca(178, 7541);
+		ob[1] = new ContaCorrente(561, 7895);
+		ob[2] = new Cliente();
+		
+		for(int i = 0; i < ob.length; i++) {
+			System.out.println(ob[i]);
+		}
+		
+		ContaCorrente cc4 = (ContaCorrente) ob[1]; 
+		System.out.println(cc4);
 		
 	}
 
